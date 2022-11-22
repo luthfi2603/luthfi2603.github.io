9b80751a4638641ab3e8475e8d4c75e5ebd07c12
luthfi2603 <luthfim904@gmail.com>
2022-11-22 22:34:11 +0700

    penambahan web portofolio

---
+++ tugas1GDSC/assets/js/tugas1.js
@@ -0,0 +1,9 @@
+function theme(para){
+    if(para == "I"){
+        document.body.style.background = "#112A46"
+        document.body.style.color = "#ACC8E5"
+    }else if(para == "II"){
+        document.body.style.background = "#ACC8E5"
+        document.body.style.color = "#112A46"
+    }
+}
